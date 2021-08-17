c8ed33bb1297b8ab949f94e1a68b47249470d378
FireFather <firefather@telenet.be>
2021-08-17 23:40:32 +0200

    Update misc.cpp
    unused void decode_fen removed

--- nnue/misc.h
+++ nnue/misc.h
@@ -95,8 +95,5 @@ INLINE uint16_t readu_le_u16(const void* p)
 	return q[0] | (q[1] << 8);
 }
 
-void decode_fen(const char* fen_str, int* player, int* castle,
-	int* fifty, int* move_number, int* piece, int* square);
-
 #define CLAMP(a, b, c) ((a) < (b) ? (b) : (a) > (c) ? (c) : (a))
 
